767495799d99e4b92794d46cc5db838e5a824ed0
wdv <wdv4758h@gmail.com>
2013-09-03 17:50:10 +0800

    rename main to BackupFile, change to run by parameters

--- backups.py
+++ backups.py
@@ -1,60 +1,67 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-file_name = sys.argv[1]
-stime = int(sys.argv[2]) if ((len(sys.argv) > 2) and sys.argv[2].isdecimal()) else 20     #secs
+import os.path, time, shutil
+from os import makedirs, getcwd, chdir
 
-def checkdir():
-    from os import makedirs
-    makedirs('backups - %s' % file_name,exist_ok=True)
+class Backups:
 
-def main():
-    checkdir()
+    def checkdir(file_name):
+        makedirs('backups - %s' % file_name,exist_ok=True)
 
-    from os import getcwd
-    pwd = getcwd()
+    def BackupFile(file_name, stime):
+        stime = int(stime) if stime.isdecimal() else 20     #secs
+        Backups.checkdir(file_name)
 
-    #change directory to backup
-    try:
-        from os import chdir
-        chdir('backups - %s' % file_name)
-    except Exception:
-        print('chdir error !')
-        pass
+        pwd = getcwd()
 
-    #get modify time
-    try:
-        pre_modify_time = open("%s.time" % file_name, "r").read()
-    except IOError:
-        pre_modify_time = ""
-        print('file time not found, program will make a new one later')
-        pass
+        #change directory to backup
+        try:
+            chdir('backups - %s' % file_name)
+        except Exception:
+            print('chdir error !')
+            pass
 
-    import os.path
-    import time
-    try:
-        while(True):
-            modify_time = os.path.getmtime('%s/%s' % (pwd, file_name))
-            if modify_time != pre_modify_time:
-                try:
-                    import shutil
-                    shutil.copy('%s/%s' % (pwd, file_name), '%s - %s' % (file_name, time.ctime(modify_time)))
-                except Exception:
-                    print('Copy file error !')
-                    pass
+        #get modify time
+        try:
+            pre_modify_time = open("%s.time" % file_name, "r").read()
+        except IOError:
+            pre_modify_time = ""
+            print('file time not found, program will make a new one later')
+            pass
 
-                try:
-                    open("%s.time" % file_name, "w").write(str(modify_time))
-                except IOError:
-                    print("Time write backup error !!!")
-                    pass
+        try:
+            while(True):
+                modify_time = os.path.getmtime('%s/%s' % (pwd, file_name))
+                if modify_time != pre_modify_time:
+                    try:
+                        shutil.copy('%s/%s' % (pwd, file_name), '%s - %s' % (file_name, time.ctime(modify_time)))
+                    except Exception:
+                        print('Copy file error !')
+                        pass
 
-            pre_modify_time = modify_time
-            time.sleep(stime)   #secs
-    except:
-        print('exit program')
-        pass
+                    try:
+                        open("%s.time" % file_name, "w").write(str(modify_time))
+                    except IOError:
+                        print("Time write backup error !!!")
+                        pass
+
+                pre_modify_time = modify_time
+                time.sleep(stime)   #secs
+        except:
+            print('exit program')
+            pass
 
 if __name__ == "__main__":
-    main()
+    import sys
+    length = len(sys.argv)
+    if (length > 2) and sys.argv[2].isdecimal():
+        file_name = sys.argv[1]
+        stime = int(sys.argv[2])
+    elif length == 2:
+        file_name = sys.argv[1]
+        stime = 20
+    else:
+        print("missing backup file")
+        exit()
+    Backups.BackupFile(file_name, stime)
